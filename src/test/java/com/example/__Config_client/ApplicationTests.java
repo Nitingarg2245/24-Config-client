75c636dde30b433853a0d19c1fd0e1148c3839c5
Nitin Garg <ngarg2260@gmail.com>
2024-08-14 15:58:29 +0530

    Initial commit

---
+++ src/test/java/com/example/__Config_client/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.__Config_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
